eb87ed2174c1495956f8d1975ec0d213af1b47f1
Veronika Chagina <90192566+Veronika-Chagina@users.noreply.github.com>
2021-10-27 18:21:57 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -3,37 +3,39 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    if plaintext.islower():
-        for i, letter in enumerate(plaintext):
+    for i, letter in enumerate(plaintext):    
+        if letter.islower():
             index = alphabet.index(letter)
-            key_index = alphabet.index(keyword[i % len(keyword)])
+            key_index = alphabet.index(keyword[i % len(keyword)].lower())
             index = (index + key_index) % len(alphabet)
             ciphertext += alphabet[index]
-    elif plaintext.isupper():
-        for i, letter in enumerate(plaintext):
+        elif letter.isupper():
             index = alphabet2.index(letter)
-            key_index = alphabet2.index(keyword[i % len(keyword)])
+            key_index = alphabet2.index(keyword[i % len(keyword)].upper())
             index = (index + key_index) % len(alphabet2)
             ciphertext += alphabet2[index]
+        else:
+            ciphertext += letter
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
-    plaintext = ""
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     alphabet2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     encrypt_text = ""
 
-    if ciphertext.islower():
-        for i, letter in enumerate(ciphertext):
+    
+    for i, letter in enumerate(ciphertext):
+        if letter.islower():
             index = alphabet.index(letter)
-            key_index = alphabet.index(keyword[i % len(keyword)])
+            key_index = alphabet.index(keyword[i % len(keyword)].lower())
             index = (index + key_index) % len(alphabet)
             encrypt_text += alphabet[index]
-    elif ciphertext.isupper():
-        for i, letter in enumerate(ciphertext):
+        elif letter.isupper():
             index = alphabet2.index(letter)
-            key_index = alphabet2.index(keyword[i % len(keyword)])
+            key_index = alphabet2.index(keyword[i % len(keyword)].upper())
             index = (index + key_index) % len(alphabet2)
             encrypt_text += alphabet2[index]
-    return plaintext
+        else:
+            encrypt_text += letter
+    return encrypt_text
