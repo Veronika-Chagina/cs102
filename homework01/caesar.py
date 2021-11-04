f8555fb2c0717e40b92753c35184a3c5599a41f0
Veronika Chagina <90192566+Veronika-Chagina@users.noreply.github.com>
2021-11-04 17:25:37 +0300

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,41 +1,33 @@
 import typing as tp
 
-
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
-    for c in plaintext:
-        if c.isupper():
-            c_unicode = ord(c)
-            c_num = ord(c) - ord("A")
-            new_num = (c_num + shift) % 26
+    for character in plaintext:
+        if character.isupper():
+            new_num = (ord(character) - ord("A") + shift) % 26
             new_unicode = new_num + ord("A")
             new_character = chr(new_unicode)
-            ciphertext = ciphertext + new_character
-        elif c.islower():
-            c_unicode = ord(c)
-            c_num = ord(c) - ord("a")
-            new_num = (c_num + shift) % 26
+            ciphertext += new_character
+        elif character.islower():
+            new_num = (ord(character) - ord("a") + shift) % 26
             new_unicode = new_num + ord("a")
             new_character = chr(new_unicode)
-            ciphertext = ciphertext + new_character
+            ciphertext += new_character
         else:
-            ciphertext += c
-    return ciphertext
-
+            ciphertext += character
+      return ciphertext
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
-    for c in ciphertext:
-        if c.isupper():
-            c_index = ord(c) - ord("A")
-            c_og_pos = (c_index - shift) % 26 + ord("A")
+    for character in ciphertext:
+        if character.isupper():
+            c_og_pos = (ord(character) - ord("A") - shift) % 26 + ord("A")
             c_og = chr(c_og_pos)
             plaintext += c_og
-        elif c.islower():
-            c_index = ord(c) - ord("a")
-            c_og_pos = (c_index - shift) % 26 + ord("a")
+        elif character.islower():
+            c_og_pos = (ord(character) - ord("a") - shift) % 26 + ord("a")
             c_og = chr(c_og_pos)
             plaintext += c_og
         else:
-            plaintext += c
+            plaintext += character
     return plaintext
