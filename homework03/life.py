e20a1a2f4715eebf25ca9cd2d74d4e91f16c996e
Veronika-Chagina <chaginav96@gmail.com>
2022-01-05 12:22:24 +0300

    i tried to fix it

--- homework03/life.py
+++ homework03/life.py
@@ -1,9 +1,8 @@
 import pathlib
 import random
 import typing as tp
-
-import pygame
-from pygame.locals import *
+from copy import deepcopy
+from pprint import pprint as pp
 
 Cell = tp.Tuple[int, int]
 Cells = tp.List[int]
@@ -17,58 +16,110 @@ class GameOfLife:
         randomize: bool = True,
         max_generations: tp.Optional[float] = float("inf"),
     ) -> None:
-        # Размер клеточного поля
         self.rows, self.cols = size
-        # Предыдущее поколение клеток
-        self.prev_generation = self.create_grid()
-        # Текущее поколение клеток
+        self.prev_generation = [[0]]
         self.curr_generation = self.create_grid(randomize=randomize)
-        # Максимальное число поколений
-        self.max_generations = max_generations
-        # Текущее число поколений
+        self.max_gens = max_generations
         self.generations = 1
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        # Copy from previous assignment
-        pass
+        grid = []
+        for i in range(self.rows):
+            current = []
+            for j in range(self.cols):
+                current.append(random.randint(0, 1) if randomize else 0)
+            grid.append(current)
+        return grid
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        # Copy from previous assignment
-        pass
+        cells = []
+        x = cell[0]
+        y = cell[1]
+        if x - 1 >= 0:
+            if y - 1 >= 0:
+                cells.append(self.curr_generation[x - 1][y - 1])
+            cells.append(self.curr_generation[x - 1][y])
+            if y + 1 < self.cols:
+                cells.append(self.curr_generation[x - 1][y + 1])
+        if y - 1 >= 0:
+            cells.append(self.curr_generation[x][y - 1])
+        if y + 1 < self.cols:
+            cells.append(self.curr_generation[x][y + 1])
+        if x + 1 < self.rows:
+            if y - 1 >= 0:
+                cells.append(self.curr_generation[x + 1][y - 1])
+            cells.append(self.curr_generation[x + 1][y])
+            if y + 1 < self.cols:
+                cells.append(self.curr_generation[x + 1][y + 1])
+        return cells
 
     def get_next_generation(self) -> Grid:
-        # Copy from previous assignment
-        pass
+        new_grid = []
+        for x in range(0, self.rows):
+            row = []
+            for y in range(0, self.cols):
+                row.append(
+                    1
+                    if sum(self.get_neighbours((x, y))) == 3
+                    or self.curr_generation[x][y] == 1
+                    and sum(self.get_neighbours((x, y))) == 2
+                    else 0
+                )
+            new_grid.append(row)
+        return new_grid
 
     def step(self) -> None:
         """
         Выполнить один шаг игры.
         """
-        pass
+        if not self.is_max_generations_exceeded and self.is_changing:
+            self.prev_generation = deepcopy(self.curr_generation)
+            self.curr_generation = self.get_next_generation()
+            self.generations += 1
 
     @property
     def is_max_generations_exceeded(self) -> bool:
         """
         Не превысило ли текущее число поколений максимально допустимое.
         """
-        pass
+        return self.generations >= self.max_gens if self.max_gens is not None else False
 
     @property
     def is_changing(self) -> bool:
         """
         Изменилось ли состояние клеток с предыдущего шага.
         """
-        pass
+        return self.curr_generation != self.prev_generation
 
     @staticmethod
     def from_file(filename: pathlib.Path) -> "GameOfLife":
         """
         Прочитать состояние клеток из указанного файла.
         """
-        pass
+        curr_gen = []
+        with open(filename, "r", encoding="utf-8") as f:
+            for line in f:
+                if len(line) > 1:
+                    curr_gen.append(list(map(int, line[0:-1])))
+        life = GameOfLife((len(curr_gen), len(curr_gen[0])))
+        life.curr_generation = curr_gen
+        return life
 
     def save(self, filename: pathlib.Path) -> None:
         """
         Сохранить текущее состояние клеток в указанный файл.
         """
-        pass
+        with open(filename, "w", encoding="utf-8") as f:
+            for i, rows in enumerate(self.curr_generation):
+                for j, elem in enumerate(rows):
+                    f.write(str(elem))
+                f.write("\n")
+
+
+if __name__ == "__main__":
+    life0 = GameOfLife.from_file(pathlib.Path("glider.txt"))
+    pp(life0.curr_generation)
+    for _ in range(4):
+        pp(life0.prev_generation)
+        pp(life0.curr_generation)
+        life0.step()
--- homework03/life_console.py
+++ homework03/life_console.py
@@ -9,14 +9,36 @@ class Console(UI):
         super().__init__(life)
 
     def draw_borders(self, screen) -> None:
-        """ Отобразить рамку. """
-        pass
+        """Отобразить рамку."""
+        screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
-        pass
+        """Отобразить состояние клеток."""
+        for y, row in enumerate(self.life.curr_generation):
+            for x, cell in enumerate(row):
+                symbol = "*" if cell == 1 else " "
+                screen.addch(y + 1, x + 1, symbol)
+    
+    def draw_grid(self, screen) -> None:
+        """Отобразить состояние клеток."""
+        screen.clear()
+        for y in range(1, self.life.rows + 1):
+            for x in range(1, self.life.cols + 1):
+                screen.addstr(y, x, " " if self.life.curr_generation[y - 1][x - 1] == 0 else "*")
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        running = True
+        while running:
+            self.draw_grid(screen)
+            self.draw_borders(screen)
+            self.life.step()
+            screen.refresh()
+            curses.napms(100)
         curses.endwin()
+
+
+if __name__ == "__main__":
+    life0 = GameOfLife((24, 80), max_generations=50)
+    ui = Console(life0)
+    ui.run()
