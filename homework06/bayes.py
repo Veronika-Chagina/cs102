7b8cbcd68c7af820272c786c40f72bb67410beb5
Veronika Chagina <90192566+Veronika-Chagina@users.noreply.github.com>
2022-05-17 11:44:01 +0300

    Update bayes.py

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -47,32 +47,3 @@ class NaiveBayesClassifier:
         print("pred", predictions)
         return accuracy_score(predictions, y_test)
 
-
-# def clean(s):
-#     translator = str.maketrans("", "", string.punctuation)
-#     return s.translate(translator)
-#
-#
-# with open("SMSSpamCollection.txt", 'r', encoding='utf-8') as f:
-#         data = list(csv.reader(f, delimiter="\t"))
-#
-#
-# X, y = [], []
-# for target, msg in data:
-#     X.append(msg)
-#     y.append(target)
-# X = [clean(x).lower() for x in X]
-#
-# X_train, y_train, X_test, y_test = X[:3900], y[:3900], X[3900:], y[3900:]
-#
-# model = NaiveBayesClassifier(alpha=1)
-# model.fit(X_train, y_train)
-# print(model.score(X_test, y_test))
-#
-# model = Pipeline([
-#     ('vectorizer', TfidfVectorizer()),
-#     ('classifier', MultinomialNB(alpha=0.05)),
-# ])
-#
-# model.fit(X_train, y_train)
-# print(model.score(X_test, y_test))
